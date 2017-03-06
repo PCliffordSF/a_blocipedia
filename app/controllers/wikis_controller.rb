7d32c4b4f8391745847451ef03d731dd7f1bf21b
Paul Clifford <paulclifford2570@gmail.com>
2017-03-06 19:13:51 +0000

    finish routes and actions

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -9,18 +9,55 @@ class WikisController < ApplicationController
     render :new
   end
 
-  def create
-  end
+   def create
+     @wiki = Wiki.new
+     @wiki.title = params[:wiki][:title]
+     @wiki.body = params[:wiki][:body]
+     @wiki.user = current_user
+
+     if @wiki.save
+       flash[:notice] = "The wiki was saved."
+       redirect_to wiki
+     else
+
+       flash.now[:alert] = "There was an error saving the wiki. Please try again."
+       render :new
+     end
+   end
 
   def show
+      @wiki = Wiki.find(params[:id])
+      render :show
   end
 
   def edit
+      @wiki = Wiki.find(params[:id])
+      render :edit
   end
 
   def update
+     @wiki = Wiki.find(params[:id])
+     @wiki.title = params[:wiki][:title]
+     @wiki.body = params[:wiki][:body]
+ 
+     if @wiki.save
+       flash[:notice] = "Post was updated."
+       redirect_to wiki
+     else
+       flash.now[:alert] = "There was an error saving the wiki. Please try again."
+       render :edit
+     end
+     
   end
 
-  def delete
+  def destroy
+     @wiki = Wiki.find(params[:id])
+     if @wiki.destroy
+       flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+       redirect_to welcome_index_path
+     else
+       flash.now[:alert] = "There was an error deleting the wiki."
+       render :show
+     end
   end
 end

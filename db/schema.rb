e35f577108d3c9a9d49e734ff27a0a51b1e08817
Paul Clifford <paulclifford2570@gmail.com>
2017-04-13 17:32:08 +0000

    frustrateder

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20170305193654) do
   create_table "wikis", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.boolean  "public"
+    t.boolean  "private"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
